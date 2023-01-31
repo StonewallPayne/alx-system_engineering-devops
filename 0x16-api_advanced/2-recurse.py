e12de08186376df56de2c8d71121c6305e8ab43c
StonewallPayne <paynestonewall@gmail.com>
2023-01-31 16:29:17 +0000

    fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ Contains one function recurse
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=''):
+def recurse(subreddit, hot_list=[]):
     """
     queries the Reddit API and returns a list containing
     the titles of all hot articles for a given subreddit
